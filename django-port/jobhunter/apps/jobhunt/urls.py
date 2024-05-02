3ad523917735549ef33e9d6daf486d73a60efe84
James Jelenko <jamesjelenko@gmail.com>
2024-05-02 07:08:45 -0700

    [ JOBHUNTER ] — [ 2024-05-02 ]: remove project to restructure following major refactor

--- django-port/jobhunter/apps/jobhunt/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-import apps.jobhunt.views as views
-
-urlpatterns = [
-    path('', views.application_list, name='application_list'),
-    path('create/', views.application_create, name='application_create'),
-    path('/update/', views.application_update, name='application_update'),
-    path('/delete/', views.application_delete, name='application_delete'),
-]
