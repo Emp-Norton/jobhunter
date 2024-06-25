3b0349efbdce3e684ab40c63ba4a57816832d54b
James Jelenko <jamesjelenko@gmail.com>
2024-06-25 02:01:14 -0700

    [ JOBHUNTER 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 ] — [ 2024-06-25 ]: update settings file

--- django-port/jobtracker/settings.py
+++ django-port/jobtracker/settings.py
@@ -128,3 +128,9 @@ STATIC_URL = 'jobtracker/static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'BUNDLE_DIR_NAME': 'bundles/',
+        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
+    }
+}
